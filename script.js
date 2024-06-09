4b470beca55f4a745c23b58a198d4d86408ba3d9
all-arm <alibegovic2004armin@gmail.com>
2024-06-09 08:40:12 +0200

    changes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 gsap.from('.img2', 1.2, {opacity:0, y:-150, delay: 0.5});
-gsap.from('.img3', 1.2, {opacity:0, y:-150, delay:1.9});
-gsap.from('.img2', 1.2, {opacity:0, y:-150, delay: 3.3});
-gsap.from('.img3', 1.2, {opacity:0, y:-150, delay:2.6});
-gsap.from('.img2', 1.2, {opacity:0, y:-150, delay: 1.2});
-gsap.from('.img3', 1.2, {opacity:0, y:-150, delay:4});
+gsap.from('.img3', 1.2, {opacity:0, y:150, delay:1.9});
+gsap.from('.img4', 1.2, {opacity:0, y:350, delay: 3.3});
+gsap.from('.img5', 1.2, {opacity:0, y:200, delay:2.6});
+gsap.from('.img6', 1.2, {opacity:0, y:-150, delay: 1.2});
+gsap.from('.img7', 1.2, {opacity:0, y:-80, delay:4});
 gsap.from('h1', 1.2, {opacity:0, y:-150, delay:4.2});
 gsap.from('h2', 1.2, {opacity:0, y:-150, delay:4.5});
 
